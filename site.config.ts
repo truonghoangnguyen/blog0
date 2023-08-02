44a425c8b9b186c1e7d1947ee3cf439822715e1c
truong hoang nguyen <truonghoangnguyen@gmail.com>
2023-08-02 15:45:44 +0700

    first commit

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '26b13ff6a4b244cabba7131c0e5a2c79',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
